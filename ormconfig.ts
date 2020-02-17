c6e98603fcff869347f2ff0881c26a2de0ed19bf
muhammad.shoaib@tenpearls.com <s@viour18>
2020-02-17 16:10:34 +0500

    orm config changes

---
+++ ormconfig.ts
@@ -0,0 +1,24 @@
+export = {
+   type: "mysql",
+   host: process.env.DB_HOST,
+   port: process.env.DB_PORT,
+   username: process.env.DB_USERNAME,
+   password: process.env.DB_PASSWORD,
+   database: process.env.DB_NAME,
+   synchronize: false,
+   logging: false,
+   entities: [
+      "src/entity/**/*.ts"
+   ],
+   migrations: [
+      "src/migration/**/*.ts"
+   ],
+   subscribers: [
+      "src/subscriber/**/*.ts"
+   ],
+   cli: {
+      "entitiesDir": "src/entity",
+      "migrationsDir": "src/migration",
+      "subscribersDir": "src/subscriber"
+   }
+}
