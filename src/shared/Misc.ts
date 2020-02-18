1632195d45ea8692bb6efcafdc7b4f751edbe1d5
muhammad.shoaib@tenpearls.com <s@viour18>
2020-02-18 12:42:07 +0500

    logging updated

--- src/shared/Misc.ts
+++ src/shared/Misc.ts
@@ -3,9 +3,9 @@ import { logger } from './Logger';
 
 export const _Err = (err: Error) => {
     if (err) {
-        logger.error(err);
+        logger.log('error',err.message);
     }
 };
 export const _Info = (method: string, url: string, body: Object) => {
-    logger.info((`method:${method} url:${url} body:${body}`));
+    logger.log('info',(`method:${method} url:${url} body:${body}`));
 }
